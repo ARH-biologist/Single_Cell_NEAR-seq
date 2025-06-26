89058ec0b18cd0744d708b3bc33759191d44ec58
Alyssa R. Hamm <alyssahammbiologist@gmail.com>
2025-06-26 16:09:50 -0400

    Add files via upload

---
+++ 2_Matrix_Prep/25.05.21_Add_all_operons.R
@@ -0,0 +1,65 @@
+#Written and updated by Alyssa R. Hamm, May 2025
+
+#Add all operons to gene matrix
+#Your dataset needs to include a column for every single operon possible
+
+
+
+#set your working diectory
+wd <- "Insert/Working/Directory/Here"
+setwd(wd)
+getwd()
+
+
+#import libraries
+library(dplyr)
+library(tidyverse)
+
+
+#####Open the csv file that has all of the operons listed on the top
+#Use Excel, the top row needs operon names starting in cell A1
+#save as operons_row.csv in your working directory
+operons <-read.csv2("operons_row.csv", sep ="," , header=TRUE)
+operons <- operons %>% 
+  as_tibble()
+
+#####Open your specific gene matrix 
+#Here, our sample name is M1 and our species is abbreviated Aa
+Aa <-read.csv2("M1_gene_matrix.csv", sep ="," , header=TRUE)
+Aa <- Aa %>% 
+  as_tibble()
+
+#####Save your barcode names
+#Open you gene matrix in Excel
+#Copy and paste the barcode names into a new Excel sheet 
+#Save as M1_barcodes.csv in your working directory
+
+
+Aa <-Aa[,-c(1)] #remove the barcodes for now
+Aa <- Aa %>% 
+  as_tibble()
+
+#Combine the two datasets
+New <-full_join(operons,Aa)
+
+
+#Since you made new columns, you may have random NAs
+#Use this line to replace any NAs with 0s
+New <- New %>% replace(is.na(.), 0)
+
+
+#Add the barcode identities back into the dataframe
+Barcodes <-read.table("M1_Barcodes.csv", sep ="," , header=TRUE)
+Barcodes <- Barcodes %>% 
+  as_tibble()
+
+
+GeneMatrix <- New %>%
+  add_column(Barcodes, .before=("SGO_operon1")) ###SGO-operon1 is the name of our first operon in the csv
+View(GeneMatrix)
+
+#Save the gene matrix
+write.csv(GeneMatrix, "M1_Gene_Matrix_Alloperons.csv")
+
+###Move on to the Seurat steps
+
---
+++ 2_Matrix_Prep/25.05.27_Trimming.R
@@ -0,0 +1,136 @@
+#Written and updated by Alyssa R. Hamm, May 2025
+
+#Now you can do some manual trimming of the data to prepare for Seurat
+#Here you change some formatting and remove rRNA counts if wanted
+
+
+#set your working diectory
+wd <- "Insert/Working/Directory/Here"
+setwd(wd)
+getwd()
+
+#import libraries
+library(tidyverse)
+library(dplyr)
+library(naniar)
+library(janitor)
+
+#import and name operon matrix
+M1 <-read.table("M1_Gene_Matrix_Alloperons.csv", sep ="," , header=TRUE)
+
+#Make a data frame
+Matrix <-data.frame(M1)
+
+#####Remove cell barcodes for now
+#Make sure you have your barcode names saved from the last script
+Matrix <-Matrix[,-c(1)] 
+
+#Ensure all number values are numeric and not characters
+Matrix[Matrix == '1'] <- 1
+Matrix[Matrix == '2'] <- 2
+Matrix[Matrix == '3'] <- 3
+Matrix[Matrix == '4'] <- 4
+Matrix[Matrix == '5'] <- 5
+Matrix[Matrix == '6'] <- 6
+Matrix[Matrix == '7'] <- 7
+Matrix[Matrix == '8'] <- 8
+Matrix[Matrix == '9'] <- 9
+Matrix[Matrix == '0'] <- 0
+Matrix1 <-Matrix #save your work
+
+#Make your data frame into a tibble so we can alter it
+Matrix2 <- Matrix1 %>% 
+  as_tibble()
+
+#########Zero out rRNA from the table (unless you want to keep it)###########
+#####This is a list of operons known to code for rRNA.
+#We also made two extra columns at the end of the .csv file with the sum of
+#Aa and Sg rRNA reads. They are also zeroed out here
+rRNA <- Matrix2[,c("SGO_operon308","SGO_operon310","SGO_operon311","SGO_operon833",
+                   "SGO_operon834","SGO_operon836","SGO_operon949","SGO_operon950","SGO_operon952",
+                   "SGO_operon1024","SGO_operon1025","SGO_operon1027","ACT74_operon1204",
+                   "ACT74_operon1459","ACT74_operon1499","ACT74_operon1820","ACT74_operon1976",
+                   "ACT74_operon2047", "Sg_rRNA", "Aa_rRNA")]
+
+mRNA <-Matrix2 %>%
+  replace_with_na(Matrix2,replace = rRNA)
+
+######Since you replaced rRNA reads with NAs,
+#Use this line to replace any NAs with 0s
+mRNA <- mRNA %>% replace(is.na(.), 0)
+
+#####Remove SGO values
+#In our analysis, we only want to look at Aa reads, so zero out Sg
+SGO <- mRNA[,c(1:1017)]
+Aa <-mRNA %>%
+  replace_with_na(mRNA,replace = SGO)
+Aa <- Aa %>% replace(is.na(.), 0)
+
+#Add the barcode identities back into the dataframe
+Barcodes <-read.table("M1_Barcodes.csv", sep ="," , header=TRUE)
+Barcodes <- Barcodes %>% 
+  as_tibble()
+
+Matrix8 <- Aa %>%
+  add_column(Barcodes, .before=("SGO_operon1"))
+
+#####Transpose
+#Subsequent steps need barcodes on top and operons on the side
+
+Matrix9 <-t(Matrix8)
+
+Matrix10 <-Matrix9
+
+Matrix10 <-data.frame(Matrix10)
+
+Matrix10[Matrix10 == '1'] <- 1
+Matrix10[Matrix10 == '2'] <- 2
+Matrix10[Matrix10 == '3'] <- 3
+Matrix10[Matrix10 == '4'] <- 4
+Matrix10[Matrix10 == '5'] <- 5
+Matrix10[Matrix10 == '6'] <- 6
+Matrix10[Matrix10 == '7'] <- 7
+Matrix10[Matrix10 == '8'] <- 8
+Matrix10[Matrix10 == '9'] <- 9
+Matrix10[Matrix10 == '0'] <- 0
+Matrix11 <-Matrix10 #save your work
+
+Matrix11 <- Matrix11 %>% 
+  as_tibble()
+Matrix11 <- Matrix11 %>% row_to_names(row_number = 1)
+
+
+Matrix11 <- mutate_all(Matrix11, function(x) as.numeric(as.character(x)))
+
+#remove barcodes with over 5000 reads
+#before doing this, ensure you have barcode names at the top
+
+Matrix12 <- Matrix11[,colSums(Matrix11) < 5001]
+
+
+
+#####Open a csv file that has all of the operons listed on the left side
+#Use Excel, the first column needs operon names starting in cell A1
+#save as operons_col.csv in your working directory
+operons <-read.csv2("operons_col.csv", sep ="," , header=FALSE)
+operons <- operons %>% 
+  as_tibble()
+
+Matrix13 <- Matrix12 %>%
+  add_column(operons, .before=("Aa4A_bc1_10_bc2_10_bc3_20")) 
+
+
+#Transpose to get into Seurat format
+Matrix14 <-t(Matrix13)
+
+Matrix15 <- Matrix14 %>% row_to_names(row_number = 1)
+
+Matrix16 <-t(Matrix15)
+
+#Save file
+write.csv2(Matrix16, "M1_QC_Matrix.txt")
+
+
+###Move on to the next R script,
+
+
